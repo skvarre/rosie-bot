480b3f66c4c7bf0131b597fbd1f622db06a635e3
Tim Olsén <skvaertill@hotmail.com>
2024-03-06 21:36:07 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+
+client.run(TOKEN)
