967673db0c127712a90e2ce2239d7885751b3222
Tim Olsén <skvaertill@hotmail.com>
2024-03-10 15:10:15 +0100

    Add barebones implementation of rep command

--- bot.py
+++ bot.py
@@ -11,12 +11,17 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents = discord.Intents.all())
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
+    global latest_author
+    if message.author == client.user:
+        return
+
     if message.content.startswith('!'):
         # split message content by spaces
         command = message.content.split(' ')
@@ -28,6 +33,26 @@ async def on_message(message):
         elif command[0] == '!voff':
             # send a message to the channel
             await message.channel.send(print_voff_message())
-    
+        elif command[0] == '!rep':
+            answer = await message.channel.send(print_rep_message())
+            await answer.add_reaction('1️⃣')
+            await answer.add_reaction('2️⃣')
+            await answer.add_reaction('❌')
+            latest_author = message.author
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if user == client.user:
+        return
+    if user == latest_author:
+        if reaction.emoji == '1️⃣':
+            await reaction.message.channel.send(check_availability())
+            await reaction.message.delete()
+        elif reaction.emoji == '2️⃣':
+            await reaction.message.channel.send("Du valde 2")
+        elif reaction.emoji == '❌':
+            await reaction.message.delete()
+
 
 client.run(TOKEN)
+
--- bot_functions.py
+++ bot_functions.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 
 def print_help_message():
     return """
@@ -11,3 +12,15 @@ def print_voff_message():
     list = ["Voff!", "Voff Voff!", "Vore gott med en bira, VOFF!", "VOFFIIIII!", "voffvoffvoff",
             ">:( Voff! (Översättning: Jag är grining)", "VOVVVVVVOO", ":D Voff! (Översättning: Jag är glad)", ":/ Voff... (Översättning: Jag är lite osäker)"]
     return list[random.randint(0, len(list)-1)]
+
+def print_rep_message():
+    return """**Rosies Rep-planerare** <:krille:1214953899497099264>
+Vill du: 
+**1.** Kolla när alla kioskmongon kan repa?
+**2.** Boka in ett rep?"""
+
+def check_availability():
+    now = datetime.now()
+    _, week, _ = now.isocalendar()
+    return "@everyone Vilka dagar kan alla repa/ses nästa vecka? Vecka " + str(week+1)
+    
